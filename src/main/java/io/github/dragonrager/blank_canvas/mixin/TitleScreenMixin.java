ef5621a7a995f209d38ecdcbd87be53ebc202739
DragonRageR <yarovan.dwnis@gmail.com>
2026-07-14 02:08:48 +0300

    MODEL IS WORK!!!!
    YEEEEEEEEEAAAAAA

--- src/main/java/io/github/dragonrager/blank_canvas/mixin/TitleScreenMixin.java
+++ src/main/java/io/github/dragonrager/blank_canvas/mixin/TitleScreenMixin.java
@@ -1,12 +1,10 @@
 package io.github.dragonrager.blank_canvas.mixin;
-
 import io.github.dragonrager.blank_canvas.BlankCanvas;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
