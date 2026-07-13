7e1bbeb23af5d22ce5faab6f293a51d95035fcf5
DragonRageR <yarovan.dwnis@gmail.com>
2026-07-14 00:19:56 +0300

    Add to tab
    Now possible to take item in creative tab

--- src/main/java/io/github/dragonrager/blank_canvas/BlankCanvasItems.java
+++ src/main/java/io/github/dragonrager/blank_canvas/BlankCanvasItems.java
@@ -1,6 +1,8 @@
 package io.github.dragonrager.blank_canvas;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -8,9 +10,13 @@ import org.quiltmc.loader.api.ModContainer;
 
 
 public class BlankCanvasItems {
-	public static final Item Smt = new Item(new Item.Settings());
+	public static final Item BC_ITEM = new Item(new Item.Settings());
 
 	public static void registerItems(ModContainer mod) {
-		Registry.register(Registries.ITEM, new Identifier("dragonrager", "smt"), Smt);
+		Registry.register(Registries.ITEM, new Identifier("blank_canvas", "bc_item"), BC_ITEM);
+
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> entries.addItem(BlankCanvasItems.BC_ITEM));
 	}
+
+
 }
