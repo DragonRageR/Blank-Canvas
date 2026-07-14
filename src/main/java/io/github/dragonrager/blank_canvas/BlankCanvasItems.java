eec31a225706e20626ec87860514bd54277a57e4
DragonRageR <136171254+DragonRageR@users.noreply.github.com>
2026-07-14 04:36:43 +0300

    Update to new version. I don't sleep all night. I want to sleep

--- src/main/java/io/github/dragonrager/blank_canvas/BlankCanvasItems.java
+++ src/main/java/io/github/dragonrager/blank_canvas/BlankCanvasItems.java
@@ -5,7 +5,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 
 
@@ -13,7 +12,7 @@ public class BlankCanvasItems {
 	public static final Item BC_ITEM = new Item(new Item.Settings());
 
 	public static void registerItems(ModContainer mod) {
-		Registry.register(Registries.ITEM, new Identifier("blank_canvas", "bc_item"), BC_ITEM);
+		Registry.register(Registries.ITEM, "blank_canvas:bc_item", BC_ITEM);
 
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> entries.addItem(BlankCanvasItems.BC_ITEM));
 	}
